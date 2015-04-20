9e22f01d431909888b9cba1a7b2dc091cb53d1eb
Ian <iantskon@gmail.com>
2015-04-20 12:18:52 -0700

    Add search form

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,36 +2,32 @@ class CampaignsController < ApplicationController
   before_action :set_campaign, only: []
 
   def index
-
   end
 
   def search
     if params[:q]
       @campaigns = Campaign.search(params[:q])
     else
-      @campaigns = Campaign.all
+      @campaigns = Cmpaign.all
     end
   end
 
   def admoda
+    start_date = params[:start_date]
+    end_date = params[:end_date]
+
+
     require 'csv'
 
-    request = Typhoeus::Request.new("https://api.admoda.com/v1/advertiser/stats/campaigns.csv?from_date=2015-03-01&to_date=2015-03-11",
+    request = Typhoeus::Request.new("https://api.admoda.com/v1/advertiser/stats/campaigns.csv?from_date="+start_date+"&to_date="+end_date,
     # request = Typhoeus::Request.new("url2 = 'https://api.admoda.com/v1/advertiser/stats/ads.csv?from_date=2015-03-01&to_date=2015-03-11&campaign_id=184622",
                                     method: :get,
                                     headers: { 'Authorization' => ENV['admoda_token'] })
     response = request.run
-    # puts response.body
-
-    # dataframe = CSV.read(response.body, headers:true)
 
     csv_text = response.body
     @csv = CSV.parse(csv_text, :headers => true)
-    # # csv.each do |row|
-    # #   # Campaign.create(campaign_name: row['Campaign Name'])
-    # #   puts row.inspect
-    # #   puts row['Campaign Name']
-    # # end
+
   end
 
   # def mmedia
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :campaigns
+    # resources :campaigns
 
   # Example resource route with options:
   #   resources :products do
